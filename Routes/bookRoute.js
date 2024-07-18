f66682ef821c1099166e2f6cf55cbcd43f62d972
reddeppa <kamathamreddeppa2@gmail.com>
2024-07-18 22:44:18 +0530

    creating booking controller

--- Routes/bookRoute.js
+++ Routes/bookRoute.js
@@ -1,7 +1,8 @@
 import express from "express"
 import { bookingtour } from "../controller/bookingController.js";
+import { authnticate } from "../controller/authnticatecontroller.js";
 
 const router = express.Router();
 
-router.post("/booking",bookingtour)
+router.post("/booking/:id",authnticate,bookingtour)
 export default router
--- controller/bookingController.js
+++ controller/bookingController.js
@@ -3,15 +3,26 @@ import tour from "../model/tourSchema.js"
 import Booking from "../model/bookingSchema.js"
 
 export const bookingtour = async(req,res)=>{
-   
+    const {username,phonenumber,time,persons}=req.body;
+    const userid=req.userId;
+    const tourid=req.params.id;
+    let user=await User.findById(userid);
+    if(!user){
+        return res.status(404).json({success:false,message:"user not found"})
+    }
+    const Tour=await tour.findById(tourid)
+    if(!Tour){
+        return res.status(403).json({success:false,message:"tour not found"})
+    }   
     try {
-        const {username,phonenumber,time,person}=req.body
+        
        const   booking = new Booking({
             username,
             phonenumber,
             time,
-            person,
-           
+            persons,
+           user:userid,
+           tours:tourid
         })
         await booking.save();
        return res.status(200).json({success:true,message:"booking successfully"})
